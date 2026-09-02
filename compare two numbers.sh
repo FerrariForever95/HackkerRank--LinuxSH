fce7bdfd800fc7a010ac980e71631a5fecc8b6d6
M.Venkata Shanmukha Viswanadh <shanmukhamarthi@gmail.com>
2026-09-02 20:09:45 +0530

    Update compare two numbers.sh

--- compare two numbers.sh
+++ compare two numbers.sh
@@ -1,11 +1,12 @@
 read a b
-if((a==b))
+
+if ((a == b))
 then
-echo "X is equal to Y"
-elif((a>b))
-then 
-echo "X is greater than Y"
-elif((a>b))
+    echo "X is equal to Y"
+elif ((a > b))
 then
-echo "X is greater than Y"
+    echo "X is greater than Y"
+elif ((b > a))
+then
+    echo "Y is greater than X"
 fi
