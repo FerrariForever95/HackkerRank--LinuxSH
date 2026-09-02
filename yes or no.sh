785cf3bf59022c03d96f1964acaa722230d83a7d
M.Venkata Shanmukha Viswanadh <shanmukhamarthi@gmail.com>
2026-09-02 20:18:03 +0530

    Update yes or no.sh

--- yes or no.sh
+++ yes or no.sh
@@ -1,8 +1,9 @@
 read c
-if($c=='y' || $c=='Y')
-then 
-echo "YES"
-elif($c=='n'|| $c=='N')
-then 
-echo "NO"
+
+if [ "$c" = "y" ] || [ "$c" = "Y" ]
+then
+    echo "YES"
+elif [ "$c" = "n" ] || [ "$c" = "N" ]
+then
+    echo "NO"
 fi
